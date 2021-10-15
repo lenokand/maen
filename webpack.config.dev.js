fe45f100aa33f26090a9498f75e1e698580dc6bf
unknown <shtanko1991@gmail.com>
2021-10-15 16:56:04 +0300

    adaptiv cart

---
+++ webpack.config.dev.js
@@ -0,0 +1,87 @@
+"use strict";
+
+var path = require('path');
+
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/app.js', './src/app.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  devServer: {
+    open: true,
+    port: 8083,
+    contentBase: path.join(__dirname, 'src'),
+    watchContentBase: true
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: 'bundle.css'
+  }), new CopyWebpackPlugin({
+    patterns: [{
+      from: './src/public'
+    }]
+  }), new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: './src/html/index.html',
+    inject: 'body'
+  }), new HtmlWebpackPlugin({
+    filename: 'cart.html',
+    template: './src/html/cart.html',
+    inject: 'body'
+  }) // new HtmlWebpackPlugin({
+  //     filename: 'cart.html',
+  //     template: './src/html/cart.html',
+  //     inject: 'body'
+  // })
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }, {
+      test: /\.(sa|sc|c)ss$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'postcss-loader'
+      }, {
+        loader: 'sass-loader',
+        options: {
+          implementation: require('sass')
+        }
+      }]
+    }, {
+      test: /\.(png|gif|jpe?g|svg|bmp|webp)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+          name: '[name].[ext]'
+        }
+      }]
+    }, {
+      test: /\.(woff2?|ttf|otf|eot)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          outputPath: 'fonts'
+        }
+      }]
+    }]
+  }
+};
